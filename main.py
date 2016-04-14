a2aa3f4cef4d03d7021d9edfacea65d50fcc69ac
zhaopu <puz474494@gmail.com>
2016-04-14 19:03:53 +0800

    delete test

--- main.py
+++ main.py
@@ -122,22 +122,6 @@ def overview():
     return show.ovewview()
 
 
-@app.route('/test')
-def test():
-    cur = g.db.execute('select cid, title, date, category, tag from contents where type = "post" order by cid desc ')
-    a = cur.fetchall()
-    tmp = []
-    for item in a:
-        cur = g.db.execute('select name from metas where cid={cid}'.format(cid=item[3]))
-        cur1 = g.db.execute('select name from metas where cid={cid}'.format(cid=item[4]))
-        tmp_item = list(item)
-        tmp_item[3] = cur.fetchall()[0][0]
-        tmp_item[4] = cur1.fetchall()[0][0]
-        tmp_item = tuple(tmp_item)
-        tmp.append(tmp_item)
-    return repr(tmp)
-
-
 @app.route('/admin/<type>')
 def admin_show(type):
     if not session.get('logged_in'):
